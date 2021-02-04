fffc917a974356898179719500449f2a1a950c8a
CS-Sauvaj <hugosimonyjungo@aol.fr>
2021-02-04 18:38:21 +0100

    gitignore and first skeleton

---
+++ src/fr/hugosimony/languagetranslator/Main.java
@@ -0,0 +1,21 @@
+package fr.hugosimony.languagetranslator;
+
+public class Main {
+
+	/*
+	 *  @author Hugo Simony-Jungo
+	 */
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Start the translator
+		 */
+		
+		// Start the translator frame
+		Translator tl = new Translator();
+		tl.setVisible(true);
+		
+	}
+	
+}
---
+++ src/fr/hugosimony/languagetranslator/Translator.java
@@ -0,0 +1,12 @@
+package fr.hugosimony.languagetranslator;
+
+import javax.swing.JFrame;
+
+public class Translator extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public Translator() {
+		
+	}
+	
+}
