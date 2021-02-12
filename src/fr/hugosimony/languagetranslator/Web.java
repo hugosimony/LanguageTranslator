3d92ceb626d88d85173a1e2caa9207ca4fb24b01
Hugo Simony <hugosimonyjungo@aol.fr>
2021-02-12 02:27:45 +0000

    Added some comments

--- src/fr/hugosimony/languagetranslator/Web.java
+++ src/fr/hugosimony/languagetranslator/Web.java
@@ -8,23 +8,26 @@ import java.net.URLConnection;
 public class Web {
 
 	public static String getHTMLText(String lfrom, String lto, String word) throws IOException {
-		URL url = new URL("https://www.linguee.fr/" + lfrom + "-" + lto + "/search?source=auto&query=" + word);
- 
+		
+		// Get the good url
+		URL url = new URL("https://www.linguee.fr/" + lfrom + "-" + lto + "/search?source=auto&query=" + word);*
+		
+		// Connect to the website
 		URLConnection con = url.openConnection();
- 
 		InputStream input = con.getInputStream();
+		
 		String text = "";
+		
+		// Save the html in the "text" variable
 		try {
-				byte[] buffer = new byte[8192];
-				int len;
- 
-				// boucle de lecture/ecriture
-			while ( (len = input.read(buffer)) > 0) {
+			byte[] buffer = new byte[8192];
+			int len;
+			while ((len = input.read(buffer)) > 0)
 				text += new String(buffer, 0, len);
-			}
 		} finally {
 			input.close();
 		}
+		
 		return text;
 	}
 	
